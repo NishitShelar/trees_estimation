fdebe0c450314e988cf1d935f46a82da85f9ee85
NishitShelar <nishitsshelar@gmail.com>
2025-04-20 20:59:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # main.py
-
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
@@ -82,4 +82,5 @@ def calculate():
 # Run Server
 # ===========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render assigns this
+    app.run(host="0.0.0.0", port=port, debug=True)
